1c04bce85f0d5244c38eaac0c88e0f9fbcd91deb
sivekPHP <sivek40@gmail.com>
2017-03-23 20:20:06 -0300

    commit inicial

---
+++ src/BarbafeitaBundle/BarbafeitaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BarbafeitaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BarbafeitaBundle extends Bundle
+{
+}
---
+++ tests/BarbafeitaBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BarbafeitaBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+}
