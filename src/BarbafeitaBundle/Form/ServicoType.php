d50eadc1b2c26a75dad3ecc01b1ece31c1c3e71c
sivekPHP <sivek40@gmail.com>
2017-03-27 21:49:53 -0300

    Alterações no CRUD

--- src/BarbafeitaBundle/Form/ServicoType.php
+++ src/BarbafeitaBundle/Form/ServicoType.php
@@ -5,6 +5,9 @@ namespace BarbafeitaBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ServicoType extends AbstractType
 {
@@ -13,7 +16,17 @@ class ServicoType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('Nome')->add('Descricao')->add('Valor')->add('Duracao');
+        $builder->add('nome')
+                ->add('descricao', TextareaType::class, array(
+                    'label'=>'Descrição do serviço', 
+                    'label_attr'=>array('class'=>'msg-erro')))
+                ->add('valor', MoneyType::class, array(
+                    'currency'=>'BRL', 
+                    'grouping'=>1, 
+                    'label'=>'Valor (Reais)'))
+                ->add('duracao', IntegerType::class, array(
+                    'label'=>'Duração (minutos)'))
+                ;
     }
     
     /**
